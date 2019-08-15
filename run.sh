4e5e0abe489736091a618efdeb7093754d4f435a
peccu <peccul@gmail.com>
2019-08-15 17:14:25 +0900

    fix wrong variable reference

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}"); pwd)
 DIRNAME=$(dirname "${BASH_SOURCE:-$0}")
 elixir=$SCRIPT_DIR/elixir.sh
-elixir sh -c "MIX_EXS=$DIRNAME/mix.exs mix run $DIRNAME/main.exs $@"
+$elixir sh -c "MIX_EXS=$DIRNAME/mix.exs mix run $DIRNAME/main.exs $@"
