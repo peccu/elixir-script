1b7c822dcb55c63fb638489696831cb1771c5f83
peccu <peccul@gmail.com>
2019-08-16 11:33:48 +0900

    fix wrong paths by add project options
    fixed paths are below.
    These paths are updated by `MIX_EXS` environment variable.
    - build_path
    - deps_path
    - lockfile

--- mix.exs
+++ mix.exs
@@ -10,8 +10,36 @@ defmodule Snippet.Mixfile do
     [
       app: :snippet,
       version: "1.0.0",
-      deps: deps()
+      deps: deps(),
+      build_path: build_path(),
+      deps_path: deps_path(),
+      lockfile: lockfile_path()
     ]
+  end
+
+  defp mix_dir do
+    # https://github.com/elixir-lang/elixir/blob/v1.9.1/lib/mix/lib/mix/cli.ex#L37
+    (System.get_env("MIX_EXS") || "mix.exs")
+    |> Path.dirname
+    |> Path.expand
+  end
+
+  # https://hexdocs.pm/mix/Mix.Project.html#build_path/1
+  defp build_path do
+    mix_dir()
+    |> Kernel.<>("/_build")
+  end
+
+  # https://hexdocs.pm/mix/Mix.Project.html#deps_path/1
+  defp deps_path do
+    mix_dir()
+    |> Kernel.<>("/deps")
+  end
+
+  # https://elixir-lang.org/getting-started/mix-otp/dependencies-and-umbrella-projects.html#umbrella-projects
+  defp lockfile_path do
+    mix_dir()
+    |> Kernel.<>("/mix.lock")
   end
 
   # mix deps.get && mix deps.compile
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}"); pwd)
 DIRNAME=$(dirname "${BASH_SOURCE:-$0}")
 elixir=$SCRIPT_DIR/elixir.sh
+# https://hexdocs.pm/mix/Mix.html#module-environment-variables
 $elixir sh -c "MIX_EXS=$DIRNAME/mix.exs mix run $DIRNAME/main.exs $@"
